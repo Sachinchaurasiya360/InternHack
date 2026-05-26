297794a6f211ba28faee5b2c71fd055fdfd65be1
Hasini2706 <m.tejohasini@gmail.com>
2026-05-26 20:28:26 +0530

    fix: correct countdown hook import path

--- client/src/module/recruiter/hr/InterviewsPage.tsx
+++ client/src/module/recruiter/hr/InterviewsPage.tsx
@@ -2,7 +2,7 @@ import { useMemo, useState } from "react";
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { motion, AnimatePresence } from "framer-motion";
 import NumberFlow from "@number-flow/react";
-import { useInterviewCountdown } from "../../hooks/useInterviewCountdown";
+import { useInterviewCountdown } from "../../../hooks/useInterviewCountdown";
 import {
   Video,
   Plus,
